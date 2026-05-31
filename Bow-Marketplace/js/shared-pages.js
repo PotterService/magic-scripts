0b0bc1622fc6a7db58d7f3b5fffd2043ae2af23c
PotterService <33551546+PotterService@users.noreply.github.com>
2026-05-31 18:48:58 -0400

    Delete Bow-Marketplace directory

--- Bow-Marketplace/js/shared-pages.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const b=document.getElementById("mobileMenuBtn"),n=document.getElementById("navBar");if(b&&n)b.addEventListener("click",()=>n.classList.toggle("open"));});
